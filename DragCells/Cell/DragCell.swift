eb457cda87659057a58af7dab0a0e64b8ff98428
Priyanka Poojara <poojarapriyanka2305@gmail.com>
2024-10-09 11:43:13 +0530

    Set data in tableview

---
+++ DragCells/Cell/DragCell.swift
@@ -0,0 +1,19 @@
+//
+//  DragCell.swift
+//  DragCells
+//
+//  Created by Priyanka on 09/10/24.
+//
+
+import UIKit
+
+class DragCell: UITableViewCell {
+
+    @IBOutlet weak var lblTitle: UILabel!
+   
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+}
--- DragCells/ViewController.swift
+++ DragCells/ViewController.swift
@@ -9,10 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tblView: UITableView!
+    
+    let arrTitle = ["A","B","C","D","E","F","G","H","I","J"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        registerTableView()
     }
+    
+    private func registerTableView() {
+        tblView.delegate = self
+        tblView.dataSource = self
+        tblView.register(UINib(nibName: "DragCell", bundle: nil), forCellReuseIdentifier: "DragCell")
+    }
+    
+}
 
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        arrTitle.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "DragCell") as? DragCell else { return UITableViewCell() }
+        cell.lblTitle.text = arrTitle[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return CGFloat(50)
+    }
 }
 
